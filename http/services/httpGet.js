3bc2dae302492dd53a7bf509606e2ee37daa9d91
Sebastián Demaría <53883705+sebdemaria@users.noreply.github.com>
2023-06-27 15:38:44 -0300

    FEATURE(#003): modify success message on saving, remove logs

--- http/services/httpGet.js
+++ http/services/httpGet.js
@@ -2,7 +2,7 @@ import { HttpClient } from "./base/httpClient";
 
 export const httpGet = async (BASE_URL, ENDPOINT, OPTIONS = null) => {
     const API_KEY = process.env.NEXT_PUBLIC_NASA_API_KEY;
-    console.log(OPTIONS);
+
     try {
         let response;
 
@@ -15,7 +15,7 @@ export const httpGet = async (BASE_URL, ENDPOINT, OPTIONS = null) => {
             : (response = await HttpClient(BASE_URL).get(
                   `${OPTIONS ? `${OPTIONS}&` : ""}api_key=${API_KEY}`
               ));
-        console.log(response);
+
         return response.data;
     } catch (err) {
         return err;
